f2e9792226261c975a7f0dd6a6dcf497fe33b745
Max Edwards <maxedwar@thoughtworks.com>
2014-11-01 19:12:34 +0000

    class not found exception

---
+++ src/bulk_loader/metrics.clj
@@ -0,0 +1,30 @@
+(ns bulk-loader.metrics
+  (:require 
+    [com.stuartsierra.component :as component]
+    [environ.core :as environ]
+    [metrics.jvm.core :refer [instrument-jvm]]
+    [metrics.reporters.graphite :as graphite]
+  )
+  (:import 
+    [java.util.concurrent TimeUnit]
+    [com.codahale.metrics MetricFilter]
+    )
+  )
+
+(def GR  
+  (graphite/reporter  
+    {:host "127.0.0.1"
+     :prefix "koomus-metrics"
+     :rate-unit TimeUnit/SECONDS
+     :duration-unit TimeUnit/MILLISECONDS
+     :filter MetricFilter/ALL}))
+
+(instrument-jvm)
+
+(defrecord Metrics []
+  component/Lifecycle
+  (start [this]
+    (graphite/start GR 10))
+  (stop [this]
+    (graphite/stop GR 10)))
+
--- src/bulk_loader/system.clj
+++ src/bulk_loader/system.clj
@@ -3,43 +3,11 @@
     [com.stuartsierra.component :as component]
     [bulk-loader.web-server :as web-server]
     [bulk-loader.redis :as redis]
+    [bulk-loader.metrics]
     [environ.core :as environ]
-    [metrics.jvm.core :refer  [instrument-jvm]]))
-
-(import '(java.util.concurrent Future TimeUnit))
-
-(require '[metrics.reporters.graphite :as graphite])
-(import '[java.util.concurrent.TimeUnit])
-(import '[com.codahale.metrics MetricFilter])
-
-(def GR  
-  (graphite/reporter  
-    {:host "127.0.0.1"
-     :prefix "koomus-metrics"
-     :rate-unit TimeUnit/SECONDS
-     :duration-unit TimeUnit/MILLISECONDS
-     :filter MetricFilter/ALL}))
-
-(graphite/start GR 10)
-
-(require '[metrics.reporters.console :as console])
-
-(def CRc  (console/reporter  {}))
-(console/start CRc 10)
-
-(require '[metrics.core :refer  [new-registry]])
-
-(def reg  (new-registry))
-
-(instrument-jvm)
-
-(require '[metrics.counters :refer  [defcounter]])
-
-(defcounter reg users-connected)
-
-(require '[metrics.counters :refer  [inc!]])
-
-(println "should havve run")
+  )
+  (:import [bulk-loader.metrics Metrics])
+  )
 
 (def components [:web-server :redis])
 
@@ -54,5 +22,7 @@
   "Constructs a component system"
   []
   (map->Bulk-Loader-System
-    {:web-server (web-server/new-web-server (environ/env :host) (environ/env :port))
-     :redis (redis/new-redis (environ/env :redis-start-cmd) (environ/env :redis-conf-path))}))
+    {
+     :web-server (web-server/new-web-server (environ/env :host) (environ/env :port))
+     :redis (redis/new-redis (environ/env :redis-start-cmd) (environ/env :redis-conf-path))
+    }))
