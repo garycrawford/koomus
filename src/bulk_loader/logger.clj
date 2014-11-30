82bbbc0dd9ea7b19fbd3efed79cbb79814ae7afa
garycrawford <bigdaddy@garycrawford.co.uk>
2014-11-30 18:51:58 +0000

    adding logging around cached versions of functions

--- src/bulk_loader/logger.clj
+++ src/bulk_loader/logger.clj
@@ -11,6 +11,7 @@
 
 (def app-name {:appname "bulk-loader"})
 (def uncached (merge {:action "uncached-access"} app-name))
+(def cached (merge {:action "cached-access"} app-name))
 (def send-message (merge {:action "send-message"} app-name))
 
 (defn- log-send-msg
@@ -29,19 +30,36 @@
     (log/debug json-msg)
     (apply f args)))
 
+(defn- log-cached-access
+  [f name args]
+  (let [msg (merge cached {:msg name})
+        json-msg (json/generate-string msg)]
+    (log/debug json-msg)
+    (apply f args)))
+
 (defn- log-uncached-build-pixels
   [f & args]
   (log-uncached-access f "bulk-loader.extractor/build-pixels" args))
 
+(defn- log-cached-build-pixels
+  [f & args]
+  (log-cached-access f "bulk-loader.extractor/build-pixels-fifo" args))
+
 (defn- log-uncached-order-files-by-slice
   [f & args]
   (log-uncached-access f "bulk-loader.extractor/order-files-by-slice" args))
+
+(defn- log-cached-order-files-by-slice
+  [f & args]
+  (log-cached-access f "bulk-loader.extractor/order-files-by-slice-fifo" args))
 
 (defn- start-logger
   []
   (log-config/set-logger!)
   (hooke/add-hook #'extractor/order-files-by-slice #'log-uncached-order-files-by-slice)
   (hooke/add-hook #'extractor/build-pixels #'log-uncached-build-pixels)
+  (hooke/add-hook #'extractor/order-files-by-slice-fifo #'log-cached-order-files-by-slice)
+  (hooke/add-hook #'extractor/build-pixels-fifo #'log-cached-build-pixels)
   (hooke/add-hook #'loader/send-msg #'log-send-msg))
 
 (defrecord Logger []
