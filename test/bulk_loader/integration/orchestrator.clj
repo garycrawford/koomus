0b09756d975afbbe1641ac3375c4cb172da8fc07
garycrawford <bigdaddy@garycrawford.co.uk>
2014-12-01 19:50:13 +0000

    moved image to avoid it being compiled into the uberjar

--- test/bulk_loader/integration/orchestrator.clj
+++ test/bulk_loader/integration/orchestrator.clj
@@ -4,6 +4,6 @@
             [bulk-loader.orchestrator :refer :all]))
 
 (fact :slow "a node should exist for each of the (* 512 512 220) pixels in the image"
-      (let [path (str (System/getProperty "user.dir") "/resources/IMG00000")
+      (let [path (str (System/getProperty "user.dir") "/test/resources/IMG00000")
             result (flatten (load-dicom path 4))]
         (reduce + result) => 1048576))
